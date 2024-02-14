24bc537a775a0b559c614abf1002e9a50e519d01
zackry <zackry@berrybyte.net>
2024-02-14 02:03:42 +0000

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import localFont from "next/font/local";
+import clsx from "clsx";
+import "./globals.css";
+
+const outfit = Outfit({
+    subsets: ["latin"],
+    variable: "--font-outfit",
+});
+
+const calSans = localFont({
+    src: "./fonts/CalSans-SemiBold.woff2",
+    variable: "--font-cal-sans",
+});
+
+export const metadata: Metadata = {
+    title: "AwesomeMY",
+    description: "Your all-in-one hub for exploring groundbreaking innovations, disruptive startups, exclusive insights, and dynamic events shaping the Malaysian tech scene.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={clsx(outfit.variable, calSans.variable, "bg-[#0C0C0C] font-sans")}>
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <main className="text-center container mx-auto px-8 flex min-h-screen flex-col items-center justify-center">
+            <h1 className="font-cal-sans text-white text-6xl tracking-wide">
+                AwesomeMY
+            </h1>
+            <p className="text-lg md:text-xl text-white/80 mt-2 tracking-wide">
+                We&apos;ve got something simmering in the background. Let the magic happen.
+            </p>
+        </main>
+    );
+}
